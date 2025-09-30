e34ef3929ed3704e88cc552eb144efbbd0235f30
chuunii <epepe8603@gmail.com>
2025-09-29 21:21:51 -0700

    Configuration commit

---
+++ .config/nvim/lua/plugins/pywal.lua
@@ -0,0 +1,19 @@
+return {
+  -- add gruvbox
+  {
+    "uZer/pywal16.nvim",
+    -- for local dev replace with:
+    -- dir = '~/your/path/pywal16.nvim',
+    config = function()
+      vim.cmd.colorscheme("pywal16")
+    end,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "pywal16",
+    },
+  },
+}
